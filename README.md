193b391672b50917ead7e63f509a19f651af5e18
Adam Bielski <adambielski90@gmail.com>
2017-10-01 20:00:22 +0200

    Initial experiments with Group Equivariant Convolutions in PyTorch

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # pytorch-gconv-experiments
-Experiments with Group Equivariant Convolutions in PyTorch
+Experiments with [Group Equivariant Convolutional Networks (T. S. Cohen, M. Welling, 2016)](https://arxiv.org/abs/1602.07576)  implemented in PyTorch.
+
+# Installation
+
+Install [GrouPy](https://github.com/adambielski/GrouPy) with PyTorch support.
+
+# MNIST
+
+Modified [MNIST PyTorch example](https://github.com/pytorch/examples/tree/master/mnist) validating my implementation of G-convolutions in PyTorch.
+
+```
+cd mnist
+python mnist.py
+```
+
+This simple example uses p4 group convolutions and plane group spatial max pooling.
+
+# CIFAR-10
+
+Experiments with ResNet implementation based by [kuangliu repository](https://github.com/kuangliu/pytorch-cifar) for CIFAR-10 with PyTorch. Training uses online data augmentation with translation and flips
+
+All planar convolutions were replaced with p4m group convolutions. The number of filters in each convolutional layer was reduced by sqrt(8) to keep similar number of parameters (following [Group Equivariant Convolutional Networks](https://arxiv.org/abs/1602.07576), section 8.2).
+
+To train the ResNet18 network run
+
+```
+cd cifar10
+python train.py --n_epochs 120 --checkpoint resnet18_p4m --lr=0.01
+```
+
+The learning rate is reduced by a factor of 0.1 after 50 and 100 epochs.
+
+After 120 epochs, the network achieves **94.22%** on test set, compared to 93.02% using planar convolutions reported [here](https://github.com/kuangliu/pytorch-cifar).
---
+++ cifar10/train.py
@@ -0,0 +1,146 @@
+'''Train CIFAR10 with PyTorch.'''
+from __future__ import print_function
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+from torch.optim.lr_scheduler import MultiStepLR
+
+import torchvision
+import torchvision.transforms as transforms
+
+import os
+import argparse
+
+from models.resnet import *
+from utils import progress_bar
+from torch.autograd import Variable
+
+
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+parser.add_argument('--n_epochs', default=350, type=int)
+parser.add_argument('--checkpoint', required=True)
+args = parser.parse_args()
+
+use_cuda = torch.cuda.is_available()
+best_acc = 0  # best test accuracy
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+n_epochs = args.n_epochs
+
+# Data
+means = (0.4914, 0.4822, 0.4465)
+print('==> Preparing data..')
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(means, (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(means, (0.2023, 0.1994, 0.2010)),
+])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+# Model
+if args.resume:
+    # Load checkpoint.
+    print('==> Resuming from checkpoint..')
+    assert os.path.isdir(args.checkpoint), 'Error: no checkpoint directory found!'
+    checkpoint = torch.load(os.path.join(args.checkpoint, 'ckpt.t7'))
+    net = checkpoint['net']
+    best_acc = checkpoint['acc']
+    start_epoch = checkpoint['epoch']
+else:
+    print('==> Building model..')
+    net = ResNet18()
+
+if use_cuda:
+    net.cuda()
+    net = torch.nn.DataParallel(net, device_ids=range(torch.cuda.device_count()))
+    cudnn.benchmark = True
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+
+# Training
+def train(epoch):
+    print('\nEpoch: %d' % epoch)
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        if use_cuda:
+            inputs, targets = inputs.cuda(), targets.cuda()
+        optimizer.zero_grad()
+        inputs, targets = Variable(inputs), Variable(targets)
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.data[0]
+        _, predicted = torch.max(outputs.data, 1)
+        total += targets.size(0)
+        correct += predicted.eq(targets.data).cpu().sum()
+
+        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+            % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+def test(epoch):
+    global best_acc
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(testloader):
+        if use_cuda:
+            inputs, targets = inputs.cuda(), targets.cuda()
+        inputs, targets = Variable(inputs, volatile=True), Variable(targets)
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+
+        test_loss += loss.data[0]
+        _, predicted = torch.max(outputs.data, 1)
+        total += targets.size(0)
+        correct += predicted.eq(targets.data).cpu().sum()
+
+        progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+            % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+    # Save checkpoint.
+    acc = 100.*correct/total
+    if acc > best_acc:
+        print('Saving..')
+        state = {
+            'net': net.module if use_cuda else net,
+            'acc': acc,
+            'epoch': epoch,
+        }
+        if not os.path.isdir(args.checkpoint):
+            os.mkdir(args.checkpoint)
+        torch.save(state, os.path.join(args.checkpoint, 'ckpt.t7'))
+        best_acc = acc
+
+milestones = [50, 100, 140]
+scheduler = MultiStepLR(optimizer, milestones, gamma=0.1)
+for epoch in range(start_epoch):
+    scheduler.step()
+
+for epoch in range(start_epoch, n_epochs):
+    scheduler.step()
+    train(epoch)
+    test(epoch)
